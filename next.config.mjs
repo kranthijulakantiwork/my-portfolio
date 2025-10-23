7b28e7c52e176281c1c15e1e2efcb127de855a9c
Kranthi Kumar Julakanti <kranthi.julakanti.work@gmail.com>
2025-10-23 20:16:41 +0530

    optimizing css

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+  experimental: {
+    optimizeCss: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+};
 
-export default nextConfig
+export default nextConfig;
